f2bd7598af30b587cb98c92b1e80b61894e61bf6
yogesh <34572252+yogeshR5@users.noreply.github.com>
2019-07-05 00:35:26 +0530

    Update index.js

--- EmployeeDetails/src/store/index.js
+++ EmployeeDetails/src/store/index.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import  thunk from 'redux-thunk'
 import rootReducer from '../reducer'
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(thunk))
+const configureStore = () => createStore(rootReducer)
 
 export default configureStore
